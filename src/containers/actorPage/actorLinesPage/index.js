f04b766db7ad286ae49be6bd25e75cc5fc85bd1e
Michel Momeyer <strihawk1213@gmail.com>
2017-10-17 22:39:25 -0400

    Added actorBlocking and actorLines container pages

---
+++ src/containers/actorPage/actorLinesPage/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class actorLinesPage extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Actor: Lines</h1>
+            </div>
+        );
+    }
+
+}
+
+export default actorLinesPage;
