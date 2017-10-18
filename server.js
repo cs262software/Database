bd166fcf44bce5bbee411e929b9ef082c3d7ff83
unknown <Ziqi@DESKTOP-CDIOJ5C>
2017-10-17 20:41:15 -0400

    Ziqi added /actor/property-notes

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(webpackDevMiddleware(compiler, {
   historyApiFallback: true,
 }));
 
-const server = app.listen(3000, function() {
+const server = app.listen(8080, function() {
   const host = server.address().address;
   const port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
