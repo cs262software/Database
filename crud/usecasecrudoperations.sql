f949516203a9a2ee1a6cfda59381b63a9558af2c
kreitsma <aelic417@comcast.net>
2017-11-03 09:11:29 -0400

    Added 3.5 and 3.6 DB, separated 8.5 and 8.6 into 2 different scripts

--- crud/usecasecrudoperations.sql
+++ crud/usecasecrudoperations.sql
@@ -8,13 +8,41 @@ SELECT FilePath FROM Play;
 
 
 --8.5DB As a stage staff I want to know where I need to put the properties.
---8.6DB As a stage staff I want to know when I need to put the properties.
---8.7DB As a stage staff I want to know when I need to take the properties out of the stage.
-
+--Use 1.0DB script to check user permissions first
 SELECT PM.DestinationLocation, L.LineNum, P.Name FROM PropMovement PM
 	INNER JOIN Prop P ON P.PropID = PM.PropID
-	INNER JOIN Line L ON PM.LineID = L.LineID
-	INNER JOIN User ON PM.UserID = (@param UserID);
+	INNER JOIN Line L ON L.LineID = (@param LineID)
+	INNER JOIN User U ON U.UserID = (@param UserID);
 		
-
+--8.6DB As a stage staff I want to know when I need to put the properties.
+--Use 1.0DB script to check user permissions first
+SELECT PM.DestinationLocation, L.LineNum, P.Name FROM PropMovement PM
+	INNER JOIN Prop P ON P.PropID = (@param PropID)
+	INNER JOIN Line L ON L.LineID = PM.LineID
+	INNER JOIN User U ON U.UserID = (@param UserID);
 	
+--8.7DB As a stage staff I want to know when I need to take the properties out of the stage.	
+--Same as 8.6DB, except 'Destination Location' will be 'Offstage Left' or something of the sort.
+	
+--3.5DB (10) As a sound technician, I want to press a button that shows me when I am supposed to play which recorded sounds.
+--Use 1.0DB script to check user permissions first
+SELECT SC.LineID, S.Name, SC.Note FROM SoundCue SC 
+	INNER JOIN Sound S ON S.SoundID = SC.SoundID
+    INNER JOIN Line L ON L.LineID = SC.LineID
+    INNER JOIN User U ON U.UserID = (@param UserID);
+
+
+--3.6DB (10) As a director, I want to be able to change the settings to view what each of the other individual sees.
+--Use 1.0DB script to check user permissions first
+UPDATE Permissions SET (@param PermissionType) = (@param 1 or 0)
+	WHERE UserID = (@param UserID);
+
+
+
+
+
+
+
+
+
+
